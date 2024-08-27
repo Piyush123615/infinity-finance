06b5a7ab656fef71d5746faee4d6790dd7d7c3f6
Rajkumar0806 <rajjadhav219@gmail.com>
2024-08-27 17:07:04 +0530

    AboutUs

--- src/component/pages/Dashboard.js
+++ src/component/pages/Dashboard.js
@@ -1,6 +1,9 @@
 
 import React from 'react';
 import Header from '../navigation/Header';
+import Footer from '../navigation/Footer';
+import LoginPage from './LoginPage';
+
 import { Route, Routes } from 'react-router-dom';
 import AboutUs from './AboutUs';
 import EnquiryPage from './EnquiryPage';
@@ -12,6 +15,12 @@ const Dashboard = () => {
   return (
     <div className="homepage">
       <Header/>
+      <HeroSection/>
+      <ServiceOverview/>
+      <Testimonials/>
+      <Footer/>
+      <AboutUs/>
+      {/* <LoginPage/> */}
       <Routes>
       <Route path='/' element={<EmpDashboardHome/>}/>
       <Route path='/aboutus' element={<AboutUs/>}/>
